a29fa436146d8442dcbd3c040566be06d9509d97
Jillian Crossley <jillianc@twitter.com>
2018-11-26 15:38:11 +0000

    quick review

--- js/api.js
+++ js/api.js
@@ -4,23 +4,6 @@ $('#forecast-button').on('click', getWeather)
 var MAPBOX_API_KEY = 'pk.eyJ1IjoiamlsbGlhbmMiLCJhIjoiY2pveDIxdmdjMWc2MDN3czN5dWp4NDRtYSJ9.LxW3zFPaNUQe6zsGuCqmjg'
 var DARKSKY_API_KEY = '84370feb5f1639ecff182057d94356de'
 
-function showWeatherSummary(weatherData) {
-  /// ???
-}
-
-function getWeatherForCoordinates(coordinateData) {
-  var coordinates = // ???
-  var longitude = // ???
-  var latitude = // ???
-
-  $.ajax({
-      url: 'https://api.darksky.net/forecast/' + DARKSKY_API_KEY + '/' + latitude + ',' + longitude + '?units=si' ,
-      type: 'GET',
-      dataType: 'jsonp',
-      success: showWeatherSummary,
-      error: function() { alert('Failed!'); },
-  });
-}
 
 function getWeather() {
     var location = // ???
@@ -29,7 +12,7 @@ function getWeather() {
     $.ajax({
         url: mapBoxLocationUrl,
         type: 'GET',
-        success: getWeatherForCoordinates,
+        success: /// ???
         error: function() { alert('Failed!'); },
     });
 }
